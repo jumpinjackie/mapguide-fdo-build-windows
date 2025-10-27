3e442f24589f45d2467515f4950bb8c853cd785f
Jackie Ng <jumpinjackie@gmail.com>
2025-10-27 23:49:40 +1100

    Update MapGuide debug build script

--- mapguide_dbg.bat
+++ mapguide_dbg.bat
@@ -1,12 +1,38 @@
-SET INSTALL_DIR=C:\Workspace\mg-4.0-install\dbg64
-SET MG_INSTALLER_DIR=C:\Workspace\mg-4.0\Installer
-SET MG_BASE_DIR=C:\Workspace\mg-4.0\MgDev
+@echo off
+REM Directory where this script resides (trailing backslash included)
+SET SCRIPT_DIR=%~dp0
+SET INSTALL_DIR=%SCRIPT_DIR%mg-install\dbg64
+SET MG_INSTALLER_DIR=%SCRIPT_DIR%Installer
+SET MG_BASE_DIR=%SCRIPT_DIR%MgDev
+SET MG_INSTANTSETUP_DIR=%SCRIPT_DIR%MgInstantSetup
 SET MG_OUTPUT_DIR=%MG_BASE_DIR%\%TYPEBUILD%
 SET MG_OUTPUT_DIR_SERVER=%MG_OUTPUT_DIR%\Server
 SET MG_OUTPUT_DIR_WEB=%MG_OUTPUT_DIR%\Web
 SET MG_OUTPUT_DIR_CSMAP=%MG_OUTPUT_DIR%\CS-Map
 SET MG_OUTPUT_DIR_PDBS=%MG_OUTPUT_DIR%\pdbs
+SET MG_VER_MAJOR=4
+SET MG_VER_MINOR=0
+SET MG_VER_BUILD=0
+SET MG_VER_MAJOR_MINOR_BUILD=%MG_VER_MAJOR%.%MG_VER_MINOR%.%MG_VER_BUILD%
+SET MG_VER_REV=0
+
+if "%MG_RELEASE_LABEL%"=="" set MG_RELEASE_LABEL=Trunk
+if "%BUILD_MAIN%"=="" set BUILD_MAIN=1
+if "%BUILD_INSTALLER%"=="" set BUILD_INSTALLER=0
+if "%BUILD_INSTANTSETUP%"=="" set BUILD_INSTANTSETUP=0
+
+set /p MG_VER_REV= < "%CD%\mapguide_%MG_VER_MAJOR%%MG_VER_MINOR%_revision.txt"
+
+echo MG_RELEASE_LABEL         = %MG_RELEASE_LABEL%
+echo BUILD_MAIN               = %BUILD_MAIN%
+echo BUILD_INSTALLER          = %BUILD_INSTALLER%
+echo BUILD_INSTANTSETUP       = %BUILD_INSTANTSETUP%
+echo MG_VER_MAJOR_MINOR_BUILD = %MG_VER_MAJOR_MINOR_BUILD%
+echo MG_VER_REV               = %MG_VER_REV%
+
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+:build_main
+
 cd /D %MG_BASE_DIR%
 call setenvironment64.bat debug
 if not "%errorlevel%"=="0" goto error
@@ -17,12 +43,26 @@ if not "%errorlevel%"=="0" goto error
 cd /D %MG_INSTALLER_DIR%
 call build.bat -a=prepare -source=%INSTALL_DIR%
 if not "%errorlevel%"=="0" goto error
+if "%BUILD_INSTALLER%"=="1" goto make_installer
+if "%BUILD_INSTANTSETUP%"=="1" goto make_instantsetup
+goto done
+:make_installer
 echo [notice]: We do not make installers in debug mode
-rem call build.bat -a=generate -source=%INSTALL_DIR%
-rem if not "%errorlevel%"=="0" goto error
-rem call build.bat -source=%INSTALL_DIR%
-rem if not "%errorlevel%"=="0" goto error
-
+if "%BUILD_INSTANTSETUP%"=="1" goto make_instantsetup
+goto done
+:make_instantsetup
+if not exist %INSTALL_DIR%\Setup mkdir %INSTALL_DIR%\Setup
+cd /D %MG_INSTANTSETUP_DIR%
+msbuild /p:Configuration=Release;Platform="Any CPU" MgInstantSetup.sln
+if not "%errorlevel%"=="0" goto error
+pushd %MG_INSTANTSETUP_DIR%\out\release
+copy /Y *.exe %INSTALL_DIR%\Setup
+copy /Y *.pdb %INSTALL_DIR%\Setup
+copy /Y *.dll %INSTALL_DIR%\Setup
+copy /Y *.config %INSTALL_DIR%\Setup
+popd
+:zip_mginstantsetup
+echo [notice]: We do not make instantsetup packages in debug mode
 goto done
 
 :error
--- mapguide_dbg_setup.bat
+++ mapguide_dbg_setup.bat
@@ -1,7 +1,12 @@
 @echo off
-SET MG_LINUX_COMMON_LIBS=C:\Workspace\mgcommon\mapguideopensource-common.tar.gz
-SET MG_BASE_DIR=C:\Workspace\mg-4.0\MgDev
-SET FDO_BUILD_SRC=C:\Workspace\fdo-build\dbg64\Fdo
+rem Root directory where this script resides (includes trailing backslash)
+SET "SCRIPT_DIR=%~dp0"
+SET MG_VER_MAJOR=4
+SET MG_VER_MINOR=0
+SET MG_VER_BUILD=0
+SET FDO_BUILD_SRC=%SCRIPT_DIR%fdo-build\dbg64\Fdo
+SET MG_BASE_DIR=%SCRIPT_DIR%MgDev
+SET MG_LINUX_COMMON_LIBS=%SCRIPT_DIR%mgcommon\mapguideopensource-common.tar.gz
 SET FDO_BASE_DIR=%MG_BASE_DIR%\Oem\FDO
 pushd %FDO_BASE_DIR%
 if exist Bin rd /S /Q Bin
@@ -14,7 +19,13 @@ popd
 pushd %FDO_BASE_DIR%\Bin
 xcopy /S /Y /I /Q %FDO_BUILD_SRC%\Bin Debug64
 popd
-pushd %MG_BASE_DIR%/Bindings
+svn info "%MG_BASE_DIR%" | perl revnum.pl > "%CD%\mapguide_%MG_VER_MAJOR%%MG_VER_MINOR%_revision.txt"
+set /p MG_VER_REV= < "%CD%\mapguide_%MG_VER_MAJOR%%MG_VER_MINOR%_revision.txt"
+pushd %MG_BASE_DIR%
+cscript updateversion.vbs /major:%MG_VER_MAJOR% /minor:%MG_VER_MINOR% /point:%MG_VER_BUILD% /build:%MG_VER_REV%
+call stampassemblies.bat %MG_VER_MAJOR%.%MG_VER_MINOR%.%MG_VER_BUILD%.%MG_VER_REV%
+popd
+pushd %MG_BASE_DIR%\Bindings
 call setup_linux_native_libs.cmd %MG_LINUX_COMMON_LIBS%
 popd
 pause
